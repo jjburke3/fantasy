a0334539efcb8e815c4aa2f25818fce35352037b
JJ Burke <jburke18@alumni.nd.edu>
2018-09-13 17:50:40 +1000

    Cleaned up sims

--- regression/simSeason.py
+++ regression/simSeason.py
@@ -22,12 +22,6 @@ with DOConnect() as tunnel:
     c, conn = connection(tunnel)
     weekRun = 13
          
-<<<<<<< HEAD
-    data = pd.read_sql("""select a.winPoints as donePoints, b.winPoints as weekPoints,
- ifnull(preDraftCapital,(select avg(preDraftCapital) from analysis.preDraftCapital)) as preDraftCapital,
- b.winPoints as predictPoints, lcase(ifnull(a.winTeam,preDraftTeam)) as winTeam, 
- ifnull(a.winSeason,preDraftYear) as winSeason, b.winWeek from analysis.preDraftCapital 
-=======
     data = pd.read_sql("""select avg(a.winPoints)
  as donePoints, 
  avg(ifnull(preDraftCapital,(select avg(preDraftCapital) from analysis.preDraftCapital))) as preDraftCapital,
@@ -35,10 +29,8 @@ with DOConnect() as tunnel:
  ifnull(a.winSeason,preDraftYear) as winSeason,
  max(a.winWeek) as maxWeek
  from analysis.preDraftCapital
-
 left join (select winSeason, winTeam, avg(winPoints) as winPoints, max(winWeek) as winWeek
 from la_liga_data.wins
-
 where winWeek <= (select max(winWeek) from la_liga_data.wins where winSeason = 2018 and winWeek <= %d)
 group by 1,2) a on a.winSeason = preDraftYear and a.winTeam = preDraftTeam
 left join la_liga_data.wins b on  ifnull(a.winSeason,preDraftYear) = b.winSeason and b.winWeek > ifnull(a.winWeek,0) and ifnull(a.winTeam,preDraftTeam) = b.winTeam
@@ -60,7 +52,6 @@ group by winTeam, winSeason""" % weekRun, con=conn)
 
     pointTotals = pd.read_sql("""select winPoints from la_liga_data.wins where winWeek < 13""",
                               con=conn)
->>>>>>> e06f730fd013b81d2b9b7bdce351bab7251f58c2
 
     
     pointAverages = pd.read_sql("""select winSeason, winTeam, avg(winPoints) as avgPoints
@@ -68,232 +59,6 @@ group by winTeam, winSeason""" % weekRun, con=conn)
                               con=conn)
     data2 = data.loc[data['winSeason'] <= 2017]
 
-<<<<<<< HEAD
-    matchups = pd.read_sql("""select matchYear, matchWeek, lcase(matchTeam) as matchTeam,
-    lcase(matchOpp) as matchOpp from la_liga_data.matchups;""", con=conn)
-    conn.close()
-
-data2 = data.loc[data['winSeason'] <= 2017]
-
-pointsMean = np.mean(data2['weekPoints'])
-pointsSd = np.std(data2['weekPoints'])
-
-def normDist(preDraftCap):
-    result = (np.random.normal(pointsMean,
-                                       pointsSd,
-                                       1)*(1-model.rsquared) +
-                      np.power(10,coefs['const']+
-                      coefs['preDraftCapital']*(preDraftCap**4))*model.rsquared)
-    return result
-
-
-X = data[['preDraftCapital']]
-Y = data['predictPoints']
-X2 = data2[['preDraftCapital']]
-X2 = X2.apply(lambda a:a**4)
-Y2 = data2['predictPoints']
-Y2 = Y2.apply(np.log10)
-X2 = sm.add_constant(X2)
-X = sm.add_constant(X)
-
-reg = LinearRegression()
-
-model = sm.OLS(Y2,X2).fit()
-print(model.summary())
-
-predictData = data.loc[data['winSeason'] == 2018]
-summaryData = {}
-for index, row in predictData.iterrows():
-    summaryData[row['winTeam']] = {}
-    summaryData[row['winTeam']]['wins'] = []
-    summaryData[row['winTeam']]['losses'] = []
-    summaryData[row['winTeam']]['ties'] = []
-    summaryData[row['winTeam']]['points'] = []
-    summaryData[row['winTeam']]['playoffs'] = []
-    summaryData[row['winTeam']]['champ'] = []
-    summaryData[row['winTeam']]['highpoints'] = []
-    summaryData[row['winTeam']]['lowpoints'] = []
-
-predicts = model.predict(X)
-
-'''for index, row in data.iterrows():
-    print(row['winSeason'], row['winWeek'], row['winTeam'], row['predictPoints'], predicts[index])
-'''
-coefs = model.params
-se = model.bse
-
-rand = np.random.normal(0,.1,1)
-print('start sim')
-#for j in range(1,1001):
-
-
-for j in range(0,10000):
-    print(j)
-    teamDict = {}
-    
-    for index, row in predictData.iterrows():
-        teamDict[row['winTeam']] = {}
-
-        teamDict[row['winTeam']]['pointTotals'] = []
-
-        teamDict[row['winTeam']]['matchup'] = []
-
-        teamDict[row['winTeam']]['wins'] = []
-
-        teamDict[row['winTeam']]['losses'] = []
-
-        teamDict[row['winTeam']]['ties'] = []
-    for i in range(1,14):
-        for index, row in predictData.iterrows():
-
-            result = normDist(row['preDraftCapital'])
-            teamDict[row['winTeam']]['pointTotals'].append(result[0])
-            matchup = matchups.loc[(matchups['matchWeek'] == i) &
-                                   (matchups['matchTeam'] == row['winTeam'])]
-            matchup = matchup.iloc[0][3]
-            
-            teamDict[row['winTeam']]['matchup'].append(matchup)
-        
-        for index, row in predictData.iterrows():
-            pointsScored = teamDict[row['winTeam']]['pointTotals'][i - 1]
-            oppPoints = teamDict[teamDict[row['winTeam']]['matchup'][i-1]]['pointTotals'][i - 1]
-            teamDict[row['winTeam']]['wins'].append(pointsScored > oppPoints)
-            teamDict[row['winTeam']]['losses'].append(pointsScored < oppPoints)
-            teamDict[row['winTeam']]['ties'].append(pointsScored == oppPoints)
-    teams = []
-    winArray = []
-    lossArray = []
-    tieArray = []
-    pointArray = []
-    for index, row in predictData.iterrows():
-        wins = sum(teamDict[row['winTeam']]['wins'])
-        losses = sum(teamDict[row['winTeam']]['losses'])
-        ties = sum(teamDict[row['winTeam']]['ties'])
-        points = sum(teamDict[row['winTeam']]['pointTotals'])
-
-        teams.append(row['winTeam'])
-        winArray.append(wins)
-        lossArray.append(losses)
-        tieArray.append(ties)
-        pointArray.append(points)
-        summaryData[row['winTeam']]['wins'].append(wins)
-        summaryData[row['winTeam']]['losses'].append(losses)
-        summaryData[row['winTeam']]['ties'].append(ties)
-        summaryData[row['winTeam']]['points'].append(points)
-
-    d = {'team' : teams, 'wins' : winArray,
-         'losses' : lossArray, 'ties' : tieArray,
-         'points' : pointArray}
-
-    df = pd.DataFrame(data=d)
-
-    #high and low points
-    df = df.sort_values(['points'], ascending=[0])
-    df = df.reset_index(drop=True)
-    for index, row in df.iterrows():
-        if index < 1:
-            highpoints = 1
-        else:
-            highpoints = 0
-        if index == 13:
-            lowpoints = 1
-        else:
-            lowpoints = 0
-        summaryData[row['team']]['highpoints'].append(highpoints)
-        summaryData[row['team']]['lowpoints'].append(lowpoints)
-
-    #playoffs
-    df = df.sort_values(['wins','losses','points'], ascending=[0,1,0])
-    df = df.reset_index(drop=True)
-    for index, row in df.iterrows():
-        if index < 6:
-            playoff = 1
-        else:
-            playoff = 0
-        summaryData[row['team']]['playoffs'].append(playoff)
-    
-    #round 1
-    scores = []
-    teams = []
-    for index, row in df[2:6].iterrows():
-        teams.append(index)
-        preDraftCap = predictData.loc[predictData['winTeam'] == row['team']]['preDraftCapital']
-        preDraftCap = preDraftCap.item()
-        scores.append(normDist(preDraftCap))
-    advanceTeams = []
-
-    if scores[3] > scores[0]:
-        advanceTeams.append(teams[3])
-    else:
-        advanceTeams.append(teams[0])
-        
-    if scores[2] > scores[1]:
-        advanceTeams.append(teams[2])
-    else:
-        advanceTeams.append(teams[1])
-
-    #round 2
-
-    scores = []
-    teams = []
-    for index, row in df[0:2].iterrows():
-        teams.append(index)
-        preDraftCap = predictData.loc[predictData['winTeam'] == row['team']]['preDraftCapital']
-        preDraftCap = preDraftCap.item()
-        scores.append(normDist(preDraftCap))
-    for index, row in df[advanceTeams[0]:advanceTeams[0]+1].iterrows():
-        teams.append(index)
-        preDraftCap = predictData.loc[predictData['winTeam'] == row['team']]['preDraftCapital']
-        preDraftCap = preDraftCap.item()
-        scores.append(normDist(preDraftCap))
-    for index, row in df[advanceTeams[1]:advanceTeams[1]+1].iterrows():
-        teams.append(index)
-        preDraftCap = predictData.loc[predictData['winTeam'] == row['team']]['preDraftCapital']
-        preDraftCap = preDraftCap.item()
-        scores.append(normDist(preDraftCap))
-
-    
-    if scores[3] > scores[0]:
-        advanceTeams.append(teams[3])
-    else:
-        advanceTeams.append(teams[0])
-        
-    if scores[2] > scores[1]:
-        advanceTeams.append(teams[2])
-    else:
-        advanceTeams.append(teams[1])
-
-    #champ
-    scores = []
-    teams = []
-    for index, row in df[advanceTeams[0]:advanceTeams[0]+1].iterrows():
-        teams.append(index)
-        preDraftCap = predictData.loc[predictData['winTeam'] == row['team']]['preDraftCapital']
-        preDraftCap = preDraftCap.item()
-        scores.append(normDist(preDraftCap))
-    for index, row in df[advanceTeams[1]:advanceTeams[1]+1].iterrows():
-        teams.append(index)
-        preDraftCap = predictData.loc[predictData['winTeam'] == row['team']]['preDraftCapital']
-        preDraftCap = preDraftCap.item()
-        scores.append(normDist(preDraftCap))
-
-    if scores[0] > scores[1]:
-        champ = teams[0]
-    else:
-        champ = teams[1]
-
-    for index, row in df.iterrows():
-        if index == champ:
-            summaryData[row['team']]['champ'].append(1)
-        else:
-            summaryData[row['team']]['champ'].append(0)
-        
-
-with DOConnect() as tunnel:
-    c, conn = connection(tunnel)        
-
-        
-=======
     pointsMean = np.mean(pointTotals['winPoints'])
     pointsSd = np.std(pointTotals['winPoints'])
 
@@ -570,7 +335,6 @@ with DOConnect() as tunnel:
 
             print(j, time.time()-start)    
         print(time.time()-start)
->>>>>>> e06f730fd013b81d2b9b7bdce351bab7251f58c2
             
 
             
@@ -604,7 +368,6 @@ with DOConnect() as tunnel:
                 (standWeek, standType, standTeam, wins, losses, tie, pointsScored, exPointAverage,
                 exWins, playoffsOdds, champOdds, highpoints, lowpoints)
                 values (%s)
-
                 on duplicate key update
                 wins = values(wins),
                 losses = values(losses),
